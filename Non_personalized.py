e6dd379af997a51440f8a6885f90c235ca5e8dd0
Gadiididi <gadi.didi85@gmail.com>
2021-01-09 20:04:37 +0200

    init

---
+++ Non_personalized.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+class NonPersonalizedRecommendation:
+    def __init__(self):
+        pass
+
+    def get_simply_recommendation(self, k):
+        pass
+
+    def get_simply_place_recommendation(self, place, k):
+        pass
+
+    def get_simply_age_recommendation(self, age, k):
+        pass
