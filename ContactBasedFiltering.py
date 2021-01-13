292799b7e38e17571a43dbe14d116c772f106748
Gadiididi <gadi.didi85@gmail.com>
2021-01-13 15:26:17 +0200

    init part 3

---
+++ ContactBasedFiltering.py
@@ -0,0 +1,9 @@
+class ContactBasedFiltering:
+    def __init__(self, books):
+        self.books = books
+
+    def build_contact_sim_metrix(self):
+        pass
+
+    def get_contact_recommendation(self, book_name, k):
+        pass
